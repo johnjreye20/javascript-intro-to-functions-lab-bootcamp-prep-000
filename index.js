7c80df764576e16b2b3b403a6af6c846555549d0
John Reyes <john.jreye@gmail.com>
2019-08-04 17:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
